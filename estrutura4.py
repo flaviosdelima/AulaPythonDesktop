30cb8eb43dffa3e7e9cf1a8fc6123884263c26f4
Flavio S.  de Lima <flavio.fatesg@yahoo.com.br>
2014-10-25 23:55:01 -0200

    Tipos de Layouts

---
+++ estrutura4.py
@@ -0,0 +1,29 @@
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import sys
+
+
+class HelloWorld(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+        layout = QHBoxLayout()
+        self.label = QLabel("Olá Mundo!!")
+        line_edit = QLineEdit()
+        button = QPushButton("Fechar")
+
+        layout.addWidget(self.label)
+        layout.addWidget(line_edit)
+        layout.addWidget(button)
+        self.setLayout(layout)
+
+        button.clicked.connect(self.close)
+        #line_edit.textChanged.connect(label.setText)
+        line_edit.textChanged.connect(self.changeTextLabel)
+
+    def changeTextLabel(self,texto):
+        self.label.setText(('Igreja - '+texto))
+
+app = QApplication(sys.argv)
+dialog = HelloWorld()
+dialog.show()
+app.exec_()
